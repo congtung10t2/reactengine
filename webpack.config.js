e6540ba83a4a96e1f24cbbf099a25d0b01f55c50
congtung10t2 <congtung10t2>
2017-12-25 05:50:10 +0800

    react typescript

--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,32 @@
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
+  template: './index.html',
   filename: 'index.html',
   inject: 'body'
 })
 const path = require('path');
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js'
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+},
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+  ]
   },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+},
 plugins: [HtmlWebpackPluginConfig]
 }
