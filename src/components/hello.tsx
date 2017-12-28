45cf2865f1b6807af115b8ce937317a460970dc8
congtung <congtung10t2@gmail.com>
2017-12-28 17:20:12 +0800

    Remove dumb file. Add demo sprite to stage

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as pixi from 'pixi.js';
 import {Stage, Sprite} from 'react-pixi';
 const SPEED = 0.005;
+const element = {
+}
 export class App extends React.Component {
   constructor() {
     super();
@@ -13,12 +15,18 @@ export class App extends React.Component {
     };
     tick();
   }
+
   render() {
     return (
         <Stage
-        width={600}
+        width={1200}
         height={800}>
-
+        <Sprite
+            image={'../assets/cupcake.png'}
+            x={100}
+            y={300}
+            scale={new pixi.Point(0.25,0.25)}
+            pivot={new pixi.Point(640/2, 577/2)}/>
         </Stage>
           
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./components/hello";
-import { Kem } from "./components/kem"
-ReactDOM.render(<App><Kem/>
+ReactDOM.render(<App>
     </App>, document.getElementById('root'));
